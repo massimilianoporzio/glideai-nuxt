d31ac00f1d2801c94798897e660370b605fafdf2
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-08-03 18:23:34 +0200

    appena prima di creare components folder

---
+++ app/layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const prismic = usePrismic();
+const { data: settings } = await useAsyncData("prismicData", () =>
+  prismic.client.getSingle("settings")
+);
+useSeoMeta({
+  title: settings.value?.data.site_title,
+  ogTitle: settings.value?.data.site_title,
+  description: settings.value?.data.meta_description,
+  ogDescription: settings.value?.data.meta_description,
+  ogImage: computed(() => prismic.asImageSrc(settings.value?.data.meta_image)),
+});
+</script>
+
+<template>
+  {{ settings }}
+
+  <slot />
+</template>
